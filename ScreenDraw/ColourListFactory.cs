28794c94aaba9d4957653bfbf4fe0179ef1b5c98
chrishoar <chrisjhoar@hotmail.com>
2021-09-03 15:53:39 +0100

    First commit

---
+++ ScreenDraw/ColourListFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScreenDraw
+{
+    public interface IColourListItem
+    {
+        string Value { get; }
+        string Text { get; }
+    }
+
+    public class ColourListItem : IColourListItem
+    {
+        public string Value { get; }
+        public string Text { get; }
+
+        public ColourListItem(string Value, string Text)
+        {
+            this.Value = Value;
+            this.Text = Text;
+        }
+    }
+
+   
+    public static class ColourListFactory
+    {
+        public static IList<IColourListItem> GetColourListObject()
+        {
+            var retList = new List<IColourListItem>();
+            retList.Add(new ColourListItem("red", "Red"));
+            retList.Add(new ColourListItem("green", "Green"));
+            retList.Add(new ColourListItem("blue", "Blue"));
+            retList.Add(new ColourListItem("yellow", "Yellow"));
+            retList.Add(new ColourListItem("purple", "Purple"));
+            retList.Add(new ColourListItem("black", "Black"));
+            retList.Add(new ColourListItem("white", "White"));
+
+            return retList;
+        }
+    }
+}
---
+++ ScreenDraw/wwwroot/js/draw.js
@@ -0,0 +1,174 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/drawHub").build();
+
+connection.on("ReceiveXYData", function (X, Y) {
+    var colour = document.getElementById("colours").options[document.getElementById("colours").selectedIndex].value;
+
+    draw(X, Y, colour);
+});
+
+connection.on("ReceiveColourData", function (colour) {
+    selectElement("colours", colour);
+});
+
+connection.on("ReceiveResetDataCommand", function (colour) {
+    Reset();
+});
+
+function selectElement(id, valueToSelect) {
+    let element = document.getElementById(id);
+    element.value = valueToSelect;
+}
+
+connection.start().then(function () {
+    //document.getElementById("sendButton").disabled = false;
+
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+
+document.getElementById("colours").addEventListener("change", function (event) {
+    var colour = document.getElementById("colours").options[document.getElementById("colours").selectedIndex].value;
+    
+    connection.invoke("ChangeColour", colour).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+var engage = false;
+var previousX = 0;
+var previousY = 0;
+
+
+document.getElementById("canvas").addEventListener("mousedown", function (event) {
+
+    Reset();
+    engage = true;       
+    event.preventDefault();
+});
+
+document.getElementById("canvas").addEventListener("mouseup", function (event) {
+
+    Reset();
+    //Push out a reset data command to all clients so the next time
+    //a draw happens the previous X and Y are set to 0 in their
+    //instance as well as this one
+
+    connection.invoke("ResetDataCommand").catch(function (err) {
+            return console.error(err.toString());
+        });
+    event.preventDefault();
+});
+
+document.getElementById("canvas").addEventListener("mouseleave", function (event) {
+
+    Reset();
+    event.preventDefault();
+});
+
+function Reset()
+{
+    previousX = 0;
+    previousY = 0;
+    engage = false;
+}
+
+document.getElementById("canvas").addEventListener("mousemove", function (event) {
+
+    if (engage == true) {
+        connection.invoke("SendXAndYData", event.clientX.toString(), event.clientY.toString())
+            .catch(function (err) {
+                return console.error(err.toString());
+            });
+
+        event.preventDefault();
+    }
+});
+
+document.getElementById("canvas").addEventListener("touchstart", function (event) {
+
+    Reset();
+    engage = true;
+    event.preventDefault();
+});
+
+document.getElementById("canvas").addEventListener("touchend", function (event) {
+
+    Reset();
+    //Push out a reset data command to all clients so the next time
+    //a draw happens the previous X and Y are set to 0 in their
+    //instance as well as this one
+
+    connection.invoke("ResetDataCommand").catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+
+
+document.getElementById("canvas").addEventListener("touchmove", function (event) {
+    if (engage == true) {
+        connection.invoke("SendXAndYData", event.changedTouches[0].clientX.toString(), event.changedTouches[0].clientY.toString())
+            .catch(function (err) {
+                return console.error(err.toString());
+            });
+
+        event.preventDefault();
+    }
+});
+
+const canvas = document.querySelector('#canvas');
+
+function clearCanvas() {
+    const context = canvas.getContext('2d');
+
+    context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+}
+
+function draw(X, Y, colour) {
+
+    
+    //Get the canvas so we can offset the x and y relative to its position
+    var rect = document.getElementById("canvas").getBoundingClientRect();
+
+    if (!canvas.getContext) {
+        return;
+    }
+    const ctx = canvas.getContext('2d');
+
+    if (previousX == 0) {
+        previousX = X - rect.x;
+    }
+
+    if (previousY == 0) {
+        previousY = Y - rect.y;
+    }
+
+    // set line stroke and line width
+    ctx.strokeStyle = colour;
+    ctx.lineWidth = 5;
+
+    X = X - rect.x;
+    Y = Y - rect.y;
+
+    // draw a red line
+
+    ctx.beginPath();
+    ctx.moveTo(previousX, previousY);
+    ctx.lineTo(X, Y);
+    ctx.stroke();
+    
+    //Store the X and Y to use in the next itteration. This
+    //is so we know where to drow the line from and to
+
+    previousX = X;
+    previousY = Y;
+
+}
+
+
+
