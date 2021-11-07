9d42679ff2aa64aa5ed1c04203d85551112c1e1c
Chris Hoar <chrisjhoar@hotmail.com>
2021-11-07 00:30:54 +0000

    Setting up unit testing

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -5,6 +5,7 @@ using ScreenDraw.Interfaces;
 using Moq;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
+using ScreenDraw.Components.Pages;
 
 namespace TestProject1
 {
@@ -18,10 +19,21 @@ namespace TestProject1
             Mock<ILogger<DrawHub>> Logger = new();
             sr.Object.Rooms = new System.Collections.Generic.List<IRoom>();
             DrawHub hub = new DrawHub(sr.Object, Logger.Object);
-
             Exception ex = Record.ExceptionAsync(() => hub.SendXAndYData("Test Room", "10X", "10Y", "Red")).Result;
 
             Assert.True(ex is NullReferenceException, "Send X and Y Failed");
+        }
+
+        [Fact]
+        public void CheckValidateFormData()
+        {
+            bool ret = false;
+            RoomList rl = new RoomList();
+            
+            ret = rl.ValidateCreateFormData();
+
+            Assert.True(ret == false, "ValidateCreaetFormData failed");
+            
         }
     }
 }
