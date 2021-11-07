9bd8c5f80021b822824bf30d390b9aeaab75e4f3
imac <chrisjhoar@hotmail.com>
2021-11-07 19:26:24 +0000

    Adding unit tests and bUnit for Blazor unit tests

---
+++ ScreenDraw/Components/Pages/Roomlist.Razor.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using ScreenDraw.Interfaces;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+using ScreenDraw.Classes;
+using System.Linq;
+
+namespace ScreenDraw.Components.Pages
+{
+    public class RoomlistBase : ComponentBase 
+    {
+        [Inject]
+        public ISketchRooms SketchRooms { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        [Inject]
+        public ILogger<RoomList> Logger { get; set; }
+        [Inject]
+        public IHttpContextAccessor HttpContextAccessor { get; set; }
+
+        public string newRoomName = string.Empty;
+        public string artistName = string.Empty;
+        public string error = string.Empty;
+
+        public RoomlistBase()
+        {
+    
+        }
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            artistName = CheckForArtistName();
+        }
+
+        private string CheckForArtistName()
+        {
+            //If the artist name has aready been entered persist that in the artist name input box
+            return string.IsNullOrEmpty(this.HttpContextAccessor.HttpContext.Session.GetString("ArtistName")) ? string.Empty : this.HttpContextAccessor.HttpContext.Session.GetString("ArtistName");
+        }
+
+        public void BeginCreateRoom()
+        {
+            try
+            {
+                if (ValidateCreateFormData() == true)
+                {
+                    CreateRoom();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+            }
+        }
+
+        private bool ValidateCreateFormData()
+        {
+            bool ok = false;
+            //Ensure a unique room name and also an artist name have been entered
+            if (string.IsNullOrEmpty(newRoomName.Trim()) == true)
+            {
+                error = "Please enter a name for the room you'd like to create";
+            }
+            else if (string.IsNullOrEmpty(artistName.Trim()) == true)
+            {
+                error = "Please enter the artist name you'd like to be known as";
+            }
+            else if (SketchRooms.Rooms.Where(r => r.Name == newRoomName).Count() > 0)
+            {
+                error = "A room aleady exists with that name. Please try a different one.";
+            }
+            else
+            {
+                ok = true;
+            }
+
+            return ok;
+        }
+
+        private void CreateRoom()
+        {
+            SketchRooms.Rooms.Add(new Room { Name = newRoomName, Artists = new List<IArtist>() });
+            StringBuilder url = new StringBuilder()
+                .Append("SketchRoom?RoomName=")
+                .Append(newRoomName)
+                .Append("&ArtistName=")
+                .Append(artistName);
+
+            this.NavigationManager.NavigateTo(url.ToString(), true); ;
+        }
+
+        public void BeginRedirectToRoom(string roomName)
+        {
+            try
+            {
+                if (ValidateRedirectFormData(roomName) == true)
+                {
+                    RedirectToRoom(roomName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+            }
+        }
+
+        private bool ValidateRedirectFormData(string roomName)
+        {
+            bool ok = false;
+            //Ensure and artist name has been entered
+            if (string.IsNullOrEmpty(artistName.Trim()) == true)
+            {
+                error = "Please enter an artist name you'd like to be known as";
+            }
+            else
+            {
+                ok = true;
+            }
+
+            return ok;
+        }
+
+        private void RedirectToRoom(string roomName)
+        {
+            StringBuilder url = new StringBuilder()
+                .Append("SketchRoom?RoomName=")
+                .Append(roomName)
+                .Append("&ArtistName=")
+                .Append(artistName);
+
+            this.NavigationManager.NavigateTo(url.ToString(), true); ;
+        }
+    }
+}
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -6,34 +6,66 @@ using Moq;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using ScreenDraw.Components.Pages;
+using Bunit;
+using ScreenDraw.Components.Pages;
+using ScreenDraw.Classes;
+using Microsoft.AspNetCore.Http;
 
 namespace TestProject1
 {
     public class UnitTest1
     {
-        [Fact]
-        public void CatchException()
-        {
 
-            Mock<ISketchRooms> sr = new ();
-            Mock<ILogger<DrawHub>> Logger = new();
-            sr.Object.Rooms = new System.Collections.Generic.List<IRoom>();
-            DrawHub hub = new DrawHub(sr.Object, Logger.Object);
-            Exception ex = Record.ExceptionAsync(() => hub.SendXAndYData("Test Room", "10X", "10Y", "Red")).Result;
-
-            Assert.True(ex is NullReferenceException, "Send X and Y Failed");
-        }
 
         [Fact]
-        public void CheckValidateFormData()
+        public void ValidateCreateRoomDataFails()
         {
-            bool ret = false;
-            RoomList rl = new RoomList();
-            
-            ret = rl.ValidateCreateFormData();
 
-            Assert.True(ret == false, "ValidateCreaetFormData failed");
-            
+            var rl = new RoomlistBase();
+            rl.artistName = string.Empty;
+            rl.newRoomName = string.Empty;
+            rl.BeginCreateRoom();
+
+            Assert.True(rl.error == "Please enter a name for the room you'd like to create", "BeginCreateRoom with no room name or artist name failed to error");
+
+            rl.artistName = "Test Artist";
+            rl.newRoomName = string.Empty;
+            rl.BeginCreateRoom();
+            Assert.True(rl.error == "Please enter a name for the room you'd like to create", "BeginCreateToom with no room name failed to error");
+
+            rl.artistName = string.Empty;
+            rl.newRoomName = "Test Room";
+            rl.BeginCreateRoom();
+            Assert.True(rl.error == "Please enter the artist name you'd like to be known as", "BeginCreateToom with no room name failed to error");
+
+
+            rl.SketchRooms = new SketchRooms();
+            rl.SketchRooms.Rooms = new System.Collections.Generic.List<IRoom>();
+            rl.SketchRooms.Rooms.Add(new Room() { Name = "Test Room" });
+            rl.artistName = "Test Artist";
+            rl.newRoomName = "Test Room";
+            rl.BeginCreateRoom();
+            Assert.True(rl.error == "A room aleady exists with that name. Please try a different one.", "BeginCreateRoom with duplicate room name failed to error");
+
+            //TODO - Write a test to check the output if all inputs are correct.
+            //This will trigger a Navigation event
+
         }
+
+        //[Fact]
+        //public void CheckChangingArtistNameUpdatesInputField()
+        //{
+        //    HttpContextAccessor cont = new HttpContextAccessor();
+        //    using var ctx = new TestContext();
+        //    var obj = ctx.RenderComponent<RoomList>();
+
+        //    ((RoomlistBase)obj).HttpContextAccessor = cont;
+            
+
+        //    ((RoomlistBase)obj).artistName = "Test Artist";
+
+
+        //    obj.Find("artistName").MarkupMatches("<input id='artistName' value='Test Artist' />", "Updating artist name variable does not update the input field");
+        //}
     }
 }
